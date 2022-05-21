d6143c8cff08d506eaf781ac44f9a55b46fb274c
kjuiop <copying_y@naver.com>
2022-05-22 00:42:38 +0900

    [#5] 카카오 API SWAGER 연동 close

---
+++ module-core/src/main/java/org/gig/withpet/core/data/kakaoMap/KakaoMapReqDto.java
@@ -0,0 +1,25 @@
+package org.gig.withpet.core.data.kakaoMap;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.gig.withpet.core.data.kakaoMap.types.AnalyzeType;
+
+/**
+ * @author : JAKE
+ * @date : 2022/05/21
+ */
+@Getter
+@Setter
+@Builder
+public class KakaoMapReqDto {
+
+    private String query;
+    private String analyzeType;
+
+    @Builder.Default
+    private Integer page = 1;
+
+    @Builder.Default
+    private Integer size = 10;
+}
